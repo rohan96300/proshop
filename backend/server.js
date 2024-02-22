eba344d4d621348e5b1ce1eb95debe380960fd96
rohan96300 <rohan96300@gmail.com>
2024-02-22 22:43:35 +0530

    Feature: Backend Auth Completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 connectDB();
 
@@ -11,11 +13,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(urlencoded({extended: true}));
+
+//cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req,res) => {
     res.send('API is runnning');
 })
 
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
