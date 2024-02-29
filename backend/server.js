5e9f5d75b7f03713ee2415a7ac13f976381218d7
rohan96300 <rohan96300@gmail.com>
2024-02-29 23:16:57 +0530

    corrected upload product image

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,13 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/orderRoutes.js';
-import multer from 'multer';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 connectDB();
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename(req, file, cb) {
-      cb(
-        null,
-        `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-      );
-    },
-  });
-
-const upload = multer({ storage })
 
 const port = process.env.PORT || 5000;
 
@@ -47,14 +33,8 @@ app.get('/', (req,res) => {
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
-//app.use('/api/upload', uploadRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.post('/api/upload',upload.single('image') ,(req, res) => {
-    res.send({
-        message: "Image uploaded",
-        image: `/${req.file.path}`,
-      });
-})
 
 app.get('/api/config/paypal', (req, res) => res.send({
     clientId: process.env.PAYPAL_CLIENT_ID
